d24ebd114e381db36868a7ef6cd000477b133086
girivivek2002 <girivivek2002@gmail.com>
2025-09-04 21:58:32 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import './App.css'
-import Navbar from './components/Navbar';
-import NewsBoard from './components/NewsBoard';
+import Navbar from './components/Navbar.jsx';
+import NewsBoard from './components/NewsBoard.jsx';
 
 function App() {
   const [category, setCategory] = useState("general")
-  
+
   return (
     <div>
-      <Navbar setCategory={setCategory}/>
-      <NewsBoard category={category}/>
+      <Navbar setCategory={setCategory} />
+      <NewsBoard category={category} />
     </div>
 
   );
